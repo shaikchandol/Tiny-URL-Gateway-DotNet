bc09225ae665de78bedb0c7f4b30d3312505d9ce
shaikchandol <shaikchandol@users.noreply.github.com>
2026-04-02 10:16:45 +0530

    Add src/TinyUrl.Application/Behaviors/ValidationBehavior.cs

---
+++ src/TinyUrl.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+using MediatR;
+
+namespace TinyUrl.Application.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next(cancellationToken);
+
+        var context = new ValidationContext<TRequest>(request);
+        var failures = _validators
+            .Select(v => v.Validate(context))
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (failures.Any())
+            throw new ValidationException(failures);
+
+        return await next(cancellationToken);
+    }
+}
